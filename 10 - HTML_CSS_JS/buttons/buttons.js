4a2275eb2ebf24863e5ea58fb114de170f5c7c3d
Kaezuhara <ferrerpauljustin31@gmail.com>
2026-04-04 21:15:32 +0800

    Fixes

---
+++ 10 - HTML_CSS_JS/buttons/buttons.js
@@ -0,0 +1,49 @@
+// Upper Buttons
+
+upperButton1 = document.querySelector('.upperButton1');
+document.querySelector('.upperButton1').addEventListener('click', toggleButton1);
+
+function toggleButton1(){
+  if (upperButton1.classList.contains('is-toggle')){
+    upperButton1.classList.remove('is-toggle');
+  } else {
+    upperButton1.classList.add('is-toggle');
+  }
+}
+
+upperButton2 = document.querySelector('.upperButton2');
+document.querySelector('.upperButton2').addEventListener('click', toggleButton2);
+
+function toggleButton2(){
+  if (upperButton2.classList.contains('is-toggle')){
+    upperButton2.classList.remove('is-toggle');
+  } else {
+    upperButton2.classList.add('is-toggle');
+  }
+}
+
+upperButton3 = document.querySelector('.upperButton3');
+document.querySelector('.upperButton3').addEventListener('click', toggleButton3);
+
+function toggleButton3(){
+  if (upperButton3.classList.contains('is-toggle')){
+    upperButton3.classList.remove('is-toggle');
+  } else {
+    upperButton3.classList.add('is-toggle');
+  }
+}
+
+upperButton4 = document.querySelector('.upperButton4');
+document.querySelector('.upperButton4').addEventListener('click', toggleButton4);
+
+function toggleButton4(){
+  if (upperButton4.classList.contains('is-toggle')){
+    upperButton4.classList.remove('is-toggle');
+  } else {
+    upperButton4.classList.add('is-toggle');
+  }
+}
+
+// Lower Buttons
+
+// TO ADD
