cad3cecfefc8dc97b7d1c1a6df4075b2dfd17b2c
Kaezuhara <ferrerpauljustin31@gmail.com>
2026-04-04 15:42:22 +0800

    Finalized, improved redundancy

--- 10 - HTML_CSS_JS/10 - Rock Paper Scissors.js
+++ 10 - HTML_CSS_JS/10 - Rock Paper Scissors.js
@@ -1,53 +1,79 @@
-function play(userChoice){
-  displayUserChoice = document.querySelector('.displayUserChoice');
-  displayComputerChoice = document.querySelector('.displayComputerChoice');
-  displayResult = document.querySelector('.displayResult');
-  displayScore = document.querySelector ('.displayScore');
-  
+// GLOBAL VARIABLES to be used in multiple functions
+const displayUserChoice = document.querySelector('.displayUserChoice');
+const displayComputerChoice = document.querySelector('.displayComputerChoice');
+const displayResult = document.querySelector('.displayResult');
+const displayScore = document.querySelector ('.displayScore');
+const score = JSON.parse(localStorage.getItem('score')) || { win: 0, lose: 0, tie: 0 };
+
+// Display score values
+displayScore.innerHTML = `Win: ${score.win} | Lose: ${score.lose} | Tie: ${score.tie}`;
+
+// Run functions on clicking icons and reset button
+document.querySelector('.rockCont').addEventListener('click', () => play('rock'));
+document.querySelector('.paperCont').addEventListener('click', () => play('paper'));
+document.querySelector('.scissorsCont').addEventListener('click', () => play('scissors'));
+document.querySelector('.displayResetScore').addEventListener('click', resetScore);
+
+// Play function
+function play(userChoice){  
   let random = Math.random();
   let computerChoice = '';
-  let result = '';
-  const score = JSON.parse(localStorage.getItem('score')) ||
-    {
-      win: 0,
-      lose: 0,
-      tie: 0
-    };
 
-  displayUserChoice.innerHTML = `You chose ${userChoice}`;
-
+  // Computer choice identifier based on random value
   if (random < 0.33){
     computerChoice = 'rock';
-  } else if (random > 0.33 && random < 0.66){
+  } else if (random < 0.66){
     computerChoice = 'paper';
   } else {
     computerChoice = 'scissors';
   }
 
-  displayComputerChoice.innerHTML = `Computer chose ${computerChoice}`
-
+  // Evaluate result of user vs computer choice
   if (
       userChoice === 'rock' && computerChoice === 'rock' ||
       userChoice === 'paper' && computerChoice === 'paper' ||
       userChoice === 'scissors' && computerChoice === 'scissors' 
   ){
-    result = 'tie'
     score.tie++;
     displayResult.innerHTML = `Tie!`;
+    displayResult.style.color = "rgb(174, 164, 86)";
   } else if (
       userChoice === 'rock' && computerChoice === 'scissors' ||
       userChoice === 'paper' && computerChoice === 'rock' ||
       userChoice === 'scissors' && computerChoice === 'paper' 
   ){
-    result = 'win'
     score.win++;
     displayResult.innerHTML = `You win!`;
+    displayResult.style.color = "rgb(69, 156, 61)";
   } else {
-    result = 'lose'
     score.lose++;
     displayResult.innerHTML = `You lose!`;
+    displayResult.style.color = "rgb(161, 65, 65)";
   }
 
-  displayScore.innerHTML = `Win: ${score.win} | Lose: ${score.lose} | Win: ${score.tie}`;
+  // Display input and output and save the result
+  displayUserChoice.innerHTML = `You picked ${userChoice}`;
+  displayComputerChoice.innerHTML = `Computer picked ${computerChoice}`
+  displayScore.innerHTML = `Win: ${score.win} | Lose: ${score.lose} | Tie: ${score.tie}`;
+
+  // Save the updated score
   localStorage.setItem('score', JSON.stringify(score));
+}
+
+// Reset score function
+function resetScore(){
+  score.win = 0;
+  score.lose = 0;
+  score.tie = 0;
+
+  // Save the resetted score to the local storage
+  localStorage.setItem('score', JSON.stringify(score));
+
+  // Remove last user-computer's input-output display
+  displayUserChoice.innerHTML = '';
+  displayComputerChoice.innerHTML = '';
+  displayResult.innerHTML = '';
+
+  // Update the score display
+  displayScore.innerHTML = `Win: ${score.win} | Lose: ${score.lose} | Tie: ${score.tie}`;
 }
